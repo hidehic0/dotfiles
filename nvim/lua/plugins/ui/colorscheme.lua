a53100754e83a415e932e105fe63e43e6c129a3f
hidehic0 <hidehico.ooba@gmail.com>
2024-12-13 11:30:30 +0900

    colorschemeをnightfoxにした

--- nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/lua/plugins/ui/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
   {
     "navarasu/onedark.nvim",
+    enabled = false,
     lazy = true,
     event = "VimEnter",
     config = function()
@@ -9,6 +10,19 @@ return {
         transparent = true,
       })
       require("onedark").load()
+    end,
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    event = "VimEnter",
+    config = function()
+      require("nightfox").setup({
+        options = {
+          transparent = true,
+        },
+      })
+
+      vim.cmd("colorscheme nightfox")
     end,
   },
 }
