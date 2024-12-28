921080c50f0226d79e68b87d3cb6c4e348d46a0b
hidehic0 <hidehico.ooba@gmail.com>
2024-12-28 16:59:08 +0900

    ペイン移動のキーマップを追加した

--- wezterm/keymaps.lua
+++ wezterm/keymaps.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 M.keys = {
+  -- tab move keybinds
   {
     key = "c",
     mods = "LEADER",
@@ -17,6 +18,27 @@ M.keys = {
     key = "N",
     mods = "LEADER",
     action = act.ActivateTabRelative(-1)
+  },
+  -- pane move keybinds
+  {
+    key = "h",
+    mods = "LEADER",
+    action = act.ActivatePaneDirection("Left")
+  },
+  {
+    key = "l",
+    mods = "LEADER",
+    action = act.ActivatePaneDirection("Right")
+  },
+  {
+    key = "u",
+    mods = "LEADER",
+    action = act.ActivatePaneDirection("Up")
+  },
+  {
+    key = "j",
+    mods = "LEADER",
+    action = act.ActivatePaneDirection("Down")
   },
 }
 
