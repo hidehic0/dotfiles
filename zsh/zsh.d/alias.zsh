73b31bdbdeca8a8ed760336ce793ef20bf1379ba
hidehic0 <hidehico.ooba@gmail.com>
2024-12-29 18:09:32 +0900

    導入完了

--- zsh/zsh.d/alias.zsh
+++ zsh/zsh.d/alias.zsh
@@ -6,3 +6,9 @@ alias qcp="$HOME/bin/quicktest.sh"
 
 # rank mirrors
 alias rank_mirror='curl -s "https://archlinux.org/mirrorlist/?country=JP&country=AU&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - | wl-copy'
+
+# LSDeluxe alias
+alias ls="lsd"
+alias lsa="lsd -a"
+alias lsl="lsd -l"
+alias lsla="lsd -la"
