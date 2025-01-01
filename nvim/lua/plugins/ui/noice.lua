904f47c5745717e3efe17799aea8ab5340a87061
hidehic0 <hidehico.ooba@gmail.com>
2025-01-01 21:19:32 +0900

    だいだいOK プルリク切ろうかな

--- nvim/lua/plugins/ui/noice.lua
+++ nvim/lua/plugins/ui/noice.lua
@@ -4,7 +4,15 @@ return {
     event = "VeryLazy",
     dependencies = {
       "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
     },
-    opts = {}
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+        },
+      }
+    }
   }
 }
