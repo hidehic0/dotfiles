a6d4b3471791d17a237c1eaa654207a5f93a6bb0
hidehic0 <hidehico.ooba@gmail.com>
2025-01-04 20:50:42 +0900

    ojとacc用のテスト用aliasを作成した

--- zsh/zsh.d/alias.zsh
+++ zsh/zsh.d/alias.zsh
@@ -3,6 +3,10 @@ alias pypy3="$HOME/pypy3.10-v7.3.17-linux64/bin/pypy3"
 
 # quicktest command
 alias qcp="$HOME/bin/quicktest.sh"
+alias ojtp="$HOME/bin/oj_test_python.sh"
+alias ojtc="$HOME/bin/oj_test_cpp.sh"
+alias acsp="$HOME/bin/acc_submit_python.sh"
+alias acsc="$HOME/bin/acc_submit_cpp.sh"
 
 # rank mirrors
 alias rank_mirror='curl -s "https://archlinux.org/mirrorlist/?country=JP&country=AU&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - | wl-copy'
