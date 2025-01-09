81c85c6c942c740aaced9cc898ff3afe87469904
hidehic0 <hidehico.ooba@gmail.com>
2025-01-09 14:50:22 +0900

    miseのパスを追加した

--- zsh/zsh.d/paths.zsh
+++ zsh/zsh.d/paths.zsh
@@ -1,4 +1,8 @@
 # rust path
 . "$HOME/.cargo/env"
 
+# fpath
 fpath=(/home/hidehico/.local/share/sheldon/repos/github.com/zsh-users/zsh-completions/src $fpath)
+
+# mise path
+eval "$(/home/hidehico/.nix-profile/bin/mise activate zsh)"
