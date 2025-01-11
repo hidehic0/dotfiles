019e99149b5f804982d9d4d7385f914546a7b07c
hidehic0 <hidehico.ooba@gmail.com>
2025-01-11 14:38:12 +0900

    lspの設定は大体完了

---
+++ nvim/lua/plugins/config/ddc.lua
@@ -0,0 +1,56 @@
+local patch_global = vim.fn["ddc#custom#patch_global"]
+
+patch_global("ui", "pum")
+patch_global("autoCompleteEvents",{
+  'InsertEnter', 'TextChangedI', 'TextChangedP', 'CmdlineChanged',
+})
+
+patch_global("sources", {"lsp"})
+
+patch_global("sourceOptions",{
+  _ = {
+    sorters = {"sorter_fuzzy"},
+    matchers = {"matcher_fuzzy"},
+    converters = {"converter_fuzzy"}
+  },
+  lsp = {
+    mark = "[LSP]",
+    forceCompletionPattern= [[\.\w*|:\w*|->\w*]],
+  }
+})
+
+vim.fn["ddc#custom#patch_global"]("sourceParams", {
+    lsp = {
+        snippetEngine = vim.fn["denops#callback#register"](function(body)
+            vim.fn["vsnip#anonymous"](body)
+        end),
+        enableResolveItem = true,
+        enableAdditionalTextEdit = true,
+    },
+})
+
+vim.cmd[[
+inoremap <silent><expr> <TAB>
+\ pum#visible() ? '<Cmd>call pum#map#insert_relative(+1)<CR>' :
+\ (col('.') <= 1 <Bar><Bar> getline('.')[col('.') - 2] =~# '\s') ?
+\ '<TAB>' : ddc#map#manual_complete()
+inoremap <S-Tab> <Cmd>call pum#map#insert_relative(-1)<CR>
+inoremap <C-e>   <Cmd>call pum#map#cancel()<CR>
+inoremap <C-y>   <Cmd>call pum#map#confirm()<CR>
+nnoremap :       <Cmd>call CommandlinePre()<CR>:
+
+function! CommandlinePre() abort
+cnoremap <Tab>   <Cmd>call pum#map#insert_relative(+1)<CR>
+cnoremap <S-Tab> <Cmd>call pum#map#insert_relative(-1)<CR>
+cnoremap <C-e>   <Cmd>call pum#map#cancel()<CR>
+autocmd User DDCCmdlineLeave ++once call CommandlinePost()
+call ddc#enable_cmdline_completion()
+endfunction
+function! CommandlinePost() abort
+silent! cunmap <Tab>
+silent! cunmap <S-Tab>
+silent! cunmap <C-e>
+endfunction
+]]
+
+vim.fn["ddc#enable"]()
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,5 +1,43 @@
 return {
   {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "Shougo/ddc-source-lsp",
+    },
+    config = function()
+      require("plugins/config/nvim-lspconfig")
+    end
+  },
+  {
+    "Shougo/ddc.vim",
+    dependencies = {
+      -- denops
+      "vim-denops/denops.vim",
+      -- ui
+      "Shougo/ddc-ui-pum",
+      "Shougo/pum.vim",
+      -- sources
+      "Shougo/ddc-source-lsp",
+      "Shougo/ddc-source-around",
+      -- sorters
+      "tani/ddc-fuzzy",
+      -- snip
+      "hrsh7th/vim-vsnip",
+    },
+    event = {
+      "CmdlineEnter",
+      "CmdlineChanged",
+      "InsertEnter",
+      "TextChangedI",
+      "TextChangedP",
+      "TextChangedT",
+    },
+    config = function()
+      require("plugins/config/ddc")
+    end
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     event = { "VeryLazy" },
     build = ":TSUpdateSync",
