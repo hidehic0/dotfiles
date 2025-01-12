b237899c6515b5c4907748821abb1cfe7f22578f
hidehic0 <hidehico.ooba@gmail.com>
2025-01-12 21:08:19 +0900

    noiceを導入した

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -4,22 +4,34 @@ return {
     event = "VeryLazy",
     config = function()
       require("lualine").setup({})
-    end
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        signature = { enabled = false },
+      },
+      messages = {
+        enabled = false,
+      },
+    },
   },
   {
     "stevearc/oil.nvim",
     cmd = "Oil",
-    keys = {{"<leader>e","<CMD>Oil --float<CR>"}},
-    config = function ()
+    keys = { { "<leader>e", "<CMD>Oil --float<CR>" } },
+    config = function()
       require("plugins.config.oil")
-    end
+    end,
   },
   {
     "akinsho/bufferline.nvim",
     event = "VimEnter",
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function ()
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
       require("plugins.config.bufferline")
-    end
-  }
+    end,
+  },
 }
