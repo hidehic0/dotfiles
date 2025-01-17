17c3a81682a8408e22d1b97f520f45677dc4e06c
hidehic0 <hidehico.ooba@gmail.com>
2025-01-17 09:25:28 +0900

    noiceの設定をいろいろ変えた

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -9,9 +9,17 @@ return {
   {
     "folke/noice.nvim",
     event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
     opts = {
       lsp = {
         signature = { enabled = false },
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+        },
       },
       messages = {
         enabled = false,
