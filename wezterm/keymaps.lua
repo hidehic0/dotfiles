7b8813d81eede0d76368278d93813fecf0450a31
hidehic0 <hidehico.ooba@gmail.com>
2025-01-18 18:03:01 +0900

    weztermのcopymodeに入る設定を変更した

--- wezterm/keymaps.lua
+++ wezterm/keymaps.lua
@@ -7,46 +7,49 @@ M.keys = {
   {
     key = "c",
     mods = "LEADER",
-    action = act.SpawnTab 'CurrentPaneDomain',
+    action = act.SpawnTab("CurrentPaneDomain"),
   },
   {
     key = "n",
     mods = "LEADER",
-    action = act.ActivateTabRelative(1)
+    action = act.ActivateTabRelative(1),
   },
   {
     key = "N",
     mods = "LEADER",
-    action = act.ActivateTabRelative(-1)
+    action = act.ActivateTabRelative(-1),
   },
   -- pane move keybinds
   {
     key = "h",
     mods = "LEADER",
-    action = act.ActivatePaneDirection("Left")
+    action = act.ActivatePaneDirection("Left"),
   },
   {
     key = "l",
     mods = "LEADER",
-    action = act.ActivatePaneDirection("Right")
+    action = act.ActivatePaneDirection("Right"),
   },
   {
     key = "k",
     mods = "LEADER",
-    action = act.ActivatePaneDirection("Up")
+    action = act.ActivatePaneDirection("Up"),
   },
   {
     key = "j",
     mods = "LEADER",
-    action = act.ActivatePaneDirection("Down")
+    action = act.ActivatePaneDirection("Down"),
   },
   --  pane create keybinds
   { key = "%", mods = "LEADER|SHIFT", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
   { key = '"', mods = "LEADER|SHIFT", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-  -- { key = "%",
-  --   mods = "LEADER",
-  --   actions = act.SplitHorizontal { domain = 'CurrentPaneDomain' }
-  -- }
+
+  -- copy mode setting
+  {
+    key = "[",
+    mods = "LEADER",
+    action = act.ActivateCopyMode,
+  },
 }
 
 return M
