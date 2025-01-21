2fdcaa845261628c07a53bf0313518266f71cdf5
hidehic0 <hidehico.ooba@gmail.com>
2025-01-21 20:26:58 +0900

    先程のコマンドのaliasを作成した

--- zsh/zsh.d/alias.zsh
+++ zsh/zsh.d/alias.zsh
@@ -9,6 +9,10 @@ alias ojtc="$bin_dir/oj_test_cpp.sh"
 alias acsp="$bin_dir/acc_submit_python.sh"
 alias acsc="$bin_dir/acc_submit_cpp.sh"
 
+# git command alias
+alias git_push="$bin_dir/git_commands/push_cur_branch.zsh"
+alias git_pull="$bin_dir/git_commands/pull_cur_branch.zsh"
+
 # rank mirrors
 alias rank_mirror='curl -s "https://archlinux.org/mirrorlist/?country=JP&country=AU&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - | wl-copy'
 
