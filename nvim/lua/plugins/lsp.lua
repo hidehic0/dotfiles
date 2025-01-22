370607e8027d285bb35511bb75b42f85b8c4e0b5
hidehic0 <hidehico.ooba@gmail.com>
2025-01-22 20:49:35 +0900

    neodimを導入した 効果があるかは分からない

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -21,6 +21,28 @@ return {
     end,
   },
   {
+    "zbirenbaum/neodim",
+    event = "LspAttach",
+    config = function()
+      require("neodim").setup({
+        alpha = 0.75,
+        blend_color = nil,
+        hide = {
+          underline = true,
+          virtual_text = true,
+          signs = true,
+        },
+        regex = {
+          "[uU]nused",
+          "[nN]ever [rR]ead",
+          "[nN]ot [rR]ead",
+        },
+        priority = 128,
+        disable = {},
+      })
+    end,
+  },
+  {
     "matsui54/denops-popup-preview.vim",
     dependencies = {
       "vim-denops/denops.vim",
