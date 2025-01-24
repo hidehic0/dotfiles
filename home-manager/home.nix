5b7f399540b1d7805021b0dcd54c633a02aa6ed5
hidehic0 <hidehico.ooba@gmail.com>
2025-01-24 09:41:09 +0900

    home-managerでneovimも管理できるようにした

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,6 +9,11 @@ let
   username = "hidehico";
 in {
   nixpkgs = {
+    overlays = [
+      (import (builtins.fetchTarball {
+        url = "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
+      }))
+    ];
     config = {
       allowUnfree=true;
     };
@@ -21,6 +26,7 @@ in {
     stateVersion = "24.11";
     packages = with pkgs;
     [
+      aria2
       black
       delta
       emacs
@@ -34,6 +40,7 @@ in {
       lua54Packages.luacheck
       marksman
       mise
+      neovim
       online-judge-tools
       ruby
       ruff
