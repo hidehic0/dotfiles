8eb3628bcea05891d6eb90ac9850734188efc7ed
hidehic0 <hidehico.ooba@gmail.com>
2025-01-29 08:44:20 +0900

    denopsのserver設定を追加した 意味があるかは分からない

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -62,7 +62,11 @@ return {
   },
   {
     "vim-denops/denops.vim",
+    dependencies = { "vim-denops/denops-shared-server.vim" },
     event = "VimEnter",
+    config = function()
+      vim.cmd([[let g:denops_server_addr = '127.0.0.1:32123']])
+    end,
   },
   {
     "tpope/vim-fugitive",
