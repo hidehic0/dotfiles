c166c8e12d63ebb7bf0fcac14377b155dc21927e
hidehic0 <hidehico.ooba@gmail.com>
2025-01-30 16:35:14 +0900

    dduで自動previewできるようにした

--- nvim/lua/plugins/config/ddu.lua
+++ nvim/lua/plugins/config/ddu.lua
@@ -4,16 +4,14 @@ vim.fn["ddu#custom#patch_global"]({
 
   uiParams = {
     ff = {
-      autoAction = {
-        name = "preview",
-      },
-      autoResize = false,
+      startAutoAction = true,
+      autoAction = { name = "preview", delay = 0 },
       floatingBorder = "single",
       previewFloating = true,
       previewFloatingBorder = "single",
+      previewFloatingTitle = "Preview",
       prompt = "ddu.vim",
       split = "floating",
-      updateTime = 0,
     },
   },
   -- source settings
@@ -117,10 +115,20 @@ patch_local("lsp_r", {
   },
 })
 
+patch_local("lsp_d", {
+  sources = {
+    { name = "lsp_diagnostic", params = {
+      buffer = 0,
+    } },
+  },
+})
+
 -- keymaps
 vim.keymap.set("n", "<leader>ff", [[<CMD>call ddu#start(#{name: "file_rec"})<CR>]], { desc = "start ddu file_rec" })
 vim.keymap.set("n", "<leader>fg", [[<CMD>call ddu#start(#{name: "rg"})<CR>]], { desc = "start ddu ripgrep" })
-vim.keymap.set("n", "<leader>fl", [[<CMD>call ddu#start(#{name: "lines"})<CR>]], { desc = "start ddu line search" })
+vim.keymap.set("n", "<leader>fr", [[<CMD>call ddu#start(#{name: "lines"})<CR>]], { desc = "start ddu line search" })
+vim.keymap.set("n", "<leader>flr", [[<CMD>call ddu#start(#{name: "lsp_r"})<CR>]])
+vim.keymap.set("n", "<leader>fld", [[<CMD>call ddu#start(#{name: "lsp_d"})<CR>]])
 
 -- autocmds
 vim.api.nvim_create_autocmd("FileType", {
@@ -128,10 +136,14 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     -- vim.fn["ddu#ui#do_action#togglePreview"]()
     local opts = { noremap = true, silent = true, buffer = true }
-    vim.keymap.set("n", "q", [[<CMD>call ddu#ui#do_action("quit")<CR>]], opts)
+    vim.keymap.set(
+      "n",
+      "q",
+      [[<Cmd>call ddu#ui#do_action("togglePreview")<CR><CMD>call ddu#ui#do_action("quit")<CR>]],
+      opts
+    )
     vim.keymap.set("n", "<CR>", [[<CMD>call ddu#ui#do_action("itemAction")<CR>]], opts)
     vim.keymap.set("n", "i", [[<Cmd>call ddu#ui#do_action("openFilterWindow")<CR>]], opts)
-    vim.keymap.set("n", "P", [[<Cmd>call ddu#ui#do_action("togglePreview")<CR>]], opts)
     vim.keymap.set("n", "a", [[<Cmd>call ddu#ui#do_action("chooseAction")<CR>]], opts)
   end,
 })
