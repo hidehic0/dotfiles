b53b82435ce52fcc7e3d83a1af4b776efeb92d42
hidehic0 <hidehico.ooba@gmail.com>
2025-01-31 14:54:10 +0900

    Revert "dppのインストールを初める"
    This reverts commit fe20733886b268819f71b297ad7c1d278542bed3.

--- nvim/dpp.ts
+++
@@ -1,4 +0,0 @@
-import {
-  type ContextBuilder,
-  type Plugin,
-} from "jsr:@shougo/dpp-vim@~3.1.0/types";
--- nvim/install_neovim_development.zsh
+++ nvim/install_neovim_development.zsh
@@ -4,4 +4,4 @@ cd ~
 rm nvim-linux-x86_64.appimage
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage
 chmod +x nvim-linux-x86_64.appimage
-sudo mv ./nvim-linux-x86_64.appimage /usr/local/bin/nvim
+sudo mv ./nvim-linux-x86_64.appimage /sbin/nvim
