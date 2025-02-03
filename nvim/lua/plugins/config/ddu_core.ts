01f48f755c230cee46d6080ea25d287535f21619
hidehic0 <hidehico.ooba@gmail.com>
2025-02-03 17:26:11 +0900

    dduの設定もts化した

---
+++ nvim/lua/plugins/config/ddu_core.ts
@@ -0,0 +1,73 @@
+import {
+  BaseConfig,
+  type ConfigArguments,
+} from "jsr:@shougo/ddu-vim@~9.5.0/config";
+import {
+  type ActionArguments,
+  ActionFlags,
+  type DduOptions,
+} from "jsr:@shougo/ddu-vim@~9.5.0/types";
+import { type Params as FfParams } from "jsr:@shougo/ddu-ui-ff@~1.6.0";
+import { type Params as FilerParams } from "jsr:@shougo/ddu-ui-filer@~1.5.0";
+
+export class Config extends BaseConfig {
+  override config(args: ConfigArguments): Promise<void> {
+    args.contextBuilder.patchGlobal({
+      ui: "ff",
+      uiParams: {
+        ff: {
+          startAutoAction: true,
+          autoAction: { name: "preview", delay: 0 },
+          floatingBorder: "rounded",
+          previewFloating: true,
+          previewFloatingBorder: "rounded",
+          previewFloatingTitle: "Preview",
+          prompt: "ddu.vim",
+          split: "floating",
+        },
+      },
+
+      sourceOptions: {
+        _: {
+          matchers: ["matcher_fzf"],
+          sorters: ["sorter_fzf"],
+          converters: ["converter_devicon"],
+          ignoreCase: true,
+          smartCase: true,
+        },
+      },
+      sourceParams: {
+        rg: {
+          args: [
+            "--vimgrep",
+            "--json",
+            "--column",
+            "--no-heading",
+            "--color",
+            "never",
+          ],
+        },
+      },
+
+      kindOptions: {
+        _: {
+          defaultAction: "open",
+        },
+        lsp_codeAction: {
+          defaultAction: "apply",
+        },
+        action: {
+          defaultAction: "do",
+          quit: true,
+        },
+      },
+
+      filterParams: {
+        matcher_fzf: {
+          highlightMatched: "Search",
+        },
+      },
+    });
+    return Promise.resolve();
+  }
+}
