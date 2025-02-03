0e654f865cf5a5823106735b233e8d1a54a08aaf
hidehic0 <hidehico.ooba@gmail.com>
2025-02-03 21:43:38 +0900

    小修正

--- nvim/lua/plugins/config/nvim-lint.lua
+++ nvim/lua/plugins/config/nvim-lint.lua
@@ -5,6 +5,7 @@ require("lint").linters_by_ft = {
   bash = { "shellcheck" },
   sh = { "shellcheck" },
 }
+require("lint").try_lint()
 
 vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
   callback = function()
