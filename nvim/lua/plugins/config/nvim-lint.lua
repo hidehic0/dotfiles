d7e36161a90e01af9d09033b87d4e1deb0a395d2
hidehic0 <hidehico.ooba@gmail.com>
2025-02-04 21:44:56 +0900

    エラーの内容が表示されていなかったので修正

--- nvim/lua/plugins/config/nvim-lint.lua
+++ nvim/lua/plugins/config/nvim-lint.lua
@@ -1,3 +1,9 @@
+vim.diagnostic.config({
+  virtual_text = true, -- エラーメッセージを表示する
+  signs = true, -- エラーサインを表示する
+  underline = true, -- エラー箇所に下線を引く
+})
+
 require("lint").linters_by_ft = {
   lua = { "luacheck" },
   -- python = { "ruff" },
