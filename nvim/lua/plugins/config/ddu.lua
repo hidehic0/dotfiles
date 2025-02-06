4f64e46f64c59d5a3487cc0ebe0dd3a65a78ccb1
hidehic0 <hidehico.ooba@gmail.com>
2025-02-06 09:09:32 +0900

    dduでのenterキーのキーマップを変更した

--- nvim/lua/plugins/config/ddu.lua
+++ nvim/lua/plugins/config/ddu.lua
@@ -31,12 +31,7 @@ vim.api.nvim_create_autocmd("FileType", {
       [[<Cmd>call ddu#ui#do_action("closePreviewWindow")<CR><CMD>call ddu#ui#do_action("quit")<CR>]],
       opts
     )
-    vim.keymap.set(
-      "n",
-      "<CR>",
-      [[<CMD>call ddu#ui#do_action("itemAction")<CR><Cmd>call ddu#ui#do_action("closePreviewWindow")<CR><CMD>call ddu#ui#do_action("quit")<CR>]],
-      opts
-    )
+    vim.keymap.set("n", "<CR>", [[<CMD>call ddu#ui#do_action("itemAction")<CR>]], opts)
     vim.keymap.set("n", "i", [[<Cmd>call ddu#ui#do_action("openFilterWindow")<CR>]], opts)
     vim.keymap.set("n", "a", [[<Cmd>call ddu#ui#do_action("chooseAction")<CR>]], opts)
   end,
