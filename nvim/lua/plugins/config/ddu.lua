7941e3c46e2190788b7dc83843405cc7cacbda4e
hidehic0 <hidehico.ooba@gmail.com>
2025-02-11 20:27:04 +0900

    dduのchooseActionを実行した後にfilterwindowを開く設定を追加した

--- nvim/lua/plugins/config/ddu.lua
+++ nvim/lua/plugins/config/ddu.lua
@@ -33,6 +33,11 @@ vim.api.nvim_create_autocmd("FileType", {
     )
     vim.keymap.set("n", "<CR>", [[<CMD>call ddu#ui#do_action("itemAction")<CR>]], opts)
     vim.keymap.set("n", "i", [[<Cmd>call ddu#ui#do_action("openFilterWindow")<CR>]], opts)
-    vim.keymap.set("n", "a", [[<Cmd>call ddu#ui#do_action("chooseAction")<CR>]], opts)
+    vim.keymap.set(
+      "n",
+      "a",
+      [[<Cmd>call ddu#ui#do_action("chooseAction")<CR><Cmd>call ddu#ui#do_action("openFilterWindow")<CR>]],
+      opts
+    )
   end,
 })
