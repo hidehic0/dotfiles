805a68a33bb4fa3ee57ccbf42b1fd33a53e4737e
hidehic0 <hidehico.ooba@gmail.com>
2025-02-12 21:47:30 +0900

    github_cliのzshでの補完を追加した

--- zsh/zsh.d/utils.zsh
+++ zsh/zsh.d/utils.zsh
@@ -9,3 +9,10 @@ eval "$(zoxide init zsh)"
 
 # fzf
 export FZF_DEFAULT_OPTS="--ignore-case"
+
+# github cli
+if [ ! -f "$HOME/.local/lib/zsh/gh_comp.zsh" ]; then
+  gh completion -s zsh >>~/.local/lib/zsh/gh_comp.zsh
+fi
+
+source "$HOME/.local/lib/zsh/gh_comp.zsh"
