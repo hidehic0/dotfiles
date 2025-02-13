6722876011245875142ba8095f4d8e763dcdcdb9
hidehic0 <hidehico.ooba@gmail.com>
2025-02-13 21:24:43 +0900

    dialのboolをpythonのbool対応させた

--- nvim/lua/plugins/config/dial.lua
+++ nvim/lua/plugins/config/dial.lua
@@ -5,6 +5,11 @@ require("dial.config").augends:register_group({
     augend.integer.alias.hex,
     augend.date.alias["%Y/%m/%d"],
     augend.constant.alias.bool,
+    augend.constant.new({
+      elements = { "True", "False" },
+      word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+      cyclic = true, -- "or" is incremented into "and".
+    }),
   },
 })
 vim.keymap.set("n", "<C-a>", function()
