b645b8b274a9327ed1728e50471126fe180d0519
hidehic0 <hidehico.ooba@gmail.com>
2025-02-14 18:04:24 +0900

    rainbow-delimiters.nvimを導入した

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -109,4 +109,37 @@ return {
     end,
   },
   { "folke/zen-mode.nvim", event = "VeryLazy", opts = {} },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      local rainbow_delimiters = require("rainbow-delimiters")
+      require("rainbow-delimiters.setup").setup({
+        strategy = {
+          [""] = rainbow_delimiters.strategy["global"],
+          vim = rainbow_delimiters.strategy["local"],
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+          lua = "rainbow-blocks",
+        },
+        priority = {
+          [""] = 110,
+          lua = 210,
+        },
+        highlight = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterOrange",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterCyan",
+        },
+      })
+    end,
+  },
 }
