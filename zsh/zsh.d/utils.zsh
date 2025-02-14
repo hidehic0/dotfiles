86731d2b3a011244f5b117e34ee4378ef20b77ad
hidehic0 <hidehico.ooba@gmail.com>
2025-02-14 15:03:34 +0900

    小修正

--- zsh/zsh.d/utils.zsh
+++ zsh/zsh.d/utils.zsh
@@ -14,15 +14,15 @@ eval "$(zoxide init zsh)"
 export FZF_DEFAULT_OPTS="--ignore-case"
 
 # github cli
-if [ ! -f "$HOME/.local/lib/zsh/gh_comp.zsh" ]; then
+if [ ! -e "$HOME/.local/lib/zsh/gh_comp.zsh" ]; then
   gh completion -s zsh >>~/.local/lib/zsh/gh_comp.zsh
 fi
 
 source "$HOME/.local/lib/zsh/gh_comp.zsh"
 
 # jj vcs
-if [ ! -f "$HOME/.local/lib/zsh/jj_comp.zsh" ]; then
+if [ ! -e "$HOME/.local/lib/zsh/jj_comp.zsh" ]; then
   jj util completion zsh >>~/.local/lib/zsh/jj_comp.zsh
 fi
 
-source "$HOME/.local/lib/zsh/gh_comp.zsh"
+source "$HOME/.local/lib/zsh/jj_comp.zsh"
