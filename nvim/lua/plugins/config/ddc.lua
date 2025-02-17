cb4ae2eb86ff367fc9e93bcb06a48f19ad45f319
hidehic0 <hidehico.ooba@gmail.com>
2025-02-17 20:42:39 +0900

    pumのキーマップを追加した

--- nvim/lua/plugins/config/ddc.lua
+++ nvim/lua/plugins/config/ddc.lua
@@ -16,6 +16,8 @@ inoremap <silent><expr> <TAB>
 \ (col('.') <= 1 <Bar><Bar> getline('.')[col('.') - 2] =~# '\s') ?
 \ '<TAB>' : ddc#map#manual_complete()
 inoremap <S-Tab> <Cmd>call pum#map#insert_relative(-1)<CR>
+inoremap <C-n>   <Cmd>call pum#map#select_relative(+1)<CR>
+inoremap <C-p>   <Cmd>call pum#map#select_relative(-1)<CR>
 inoremap <C-e>   <Cmd>call pum#map#cancel()<CR>
 inoremap <C-y>   <Cmd>call pum#map#confirm()<CR>
 nnoremap :       <Cmd>call CommandlinePre()<CR>:
@@ -23,7 +25,10 @@ nnoremap :       <Cmd>call CommandlinePre()<CR>:
 function! CommandlinePre() abort
 cnoremap <Tab>   <Cmd>call pum#map#insert_relative(+1)<CR>
 cnoremap <S-Tab> <Cmd>call pum#map#insert_relative(-1)<CR>
+cnoremap <C-n>   <Cmd>call pum#map#select_relative(+1)<CR>
+cnoremap <C-p>   <Cmd>call pum#map#select_relative(-1)<CR>
 cnoremap <C-e>   <Cmd>call pum#map#cancel()<CR>
+cnoremap <C-y>   <Cmd>call pum#map#confirm()<CR>
 autocmd User DDCCmdlineLeave ++once call CommandlinePost()
 call ddc#enable_cmdline_completion()
 endfunction
