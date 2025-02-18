239f16bc7548628f5028db58bffe874a09afb4da
hidehic0 <hidehico.ooba@gmail.com>
2025-02-18 12:52:08 +0900

    小修正

--- nvim/install_neovim_development.zsh
+++ nvim/install_neovim_development.zsh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
 cd ~
-rm nvim-linux-x86_64.appimage
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage
-chmod +x nvim-linux-x86_64.appimage
-sudo mv ./nvim-linux-x86_64.appimage /usr/local/bin/nvim
+wget wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.tar.gz
+tar zxvf nvim-linux-x86_64.tar.gz
+
+sudo rm -rf /usr/local/bin/nvim-linux-x86_64 /usr/local/bin/nvim
+sudo mv nvim-linux-x86_64 /usr/local/bin/
+sudo ln -s /usr/local/bin/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
