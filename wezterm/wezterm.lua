cfcf9ce96afc2c033e7b2095c5b57df79beede80
hidehic0 <hidehico.ooba@gmail.com>
2025-02-27 08:24:09 +0900

    weztermのタブのタイトルを修正

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -15,4 +15,15 @@ config.keys = keymaps.keys
 config.key_tables = keymaps.key_tables
 config.enable_wayland = false
 
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local title = wezterm.truncate_right(utils.basename(tab.active_pane.foreground_process_name), max_width)
+  if title == "" then
+    title =
+      wezterm.truncate_right(utils.basename(utils.convert_home_dir(tab.active_pane.current_working_dir)), max_width)
+  end
+  return {
+    { Text = tab.tab_index + 1 .. ":" .. title },
+  }
+end)
+
 return config
