8d36959dfbaeef0238afea4eaf102bddf47b33cc
hidehic0 <hidehico.ooba@gmail.com>
2025-02-26 11:13:23 +0900

    小修正

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # home-managerをインストール
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs-unstable
 nix-channel --update
 
 # パッケージのインストール
