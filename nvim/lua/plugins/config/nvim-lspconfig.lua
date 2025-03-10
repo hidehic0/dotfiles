8407b48300638420936040799f408bc229a5364e
hidehic0 <hidehico.ooba@gmail.com>
2025-03-10 17:05:57 +0900

    neovim: nvim-lspconfigの設定をリファクタリングした

--- nvim/lua/plugins/config/nvim-lspconfig.lua
+++ nvim/lua/plugins/config/nvim-lspconfig.lua
@@ -1,65 +1,66 @@
 local lspconfig = require("lspconfig")
 local capabilities = require("ddc_source_lsp").make_client_capabilities()
 
-lspconfig["lua_ls"].setup({
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      runtime = {
-        version = "LuaJIT",
-        pathStrict = true,
-        path = { "?.lua", "?/init.lua" },
-      },
-      workspace = {
-        library = vim.list_extend(vim.api.nvim_get_runtime_file("lua", true), {
-          "${3rd}/luv/library",
-          "${3rd}/busted/library",
-          "${3rd}/luassert/library",
-        }),
-        checkThirdParty = "Disable",
-      },
-    },
-  },
-})
-
 local python_venv_path = vim.fn.system("which python"):gsub("\n", "")
 
-lspconfig["pyright"].setup({
-  capabilities = capabilities,
-  settings = {
-    pyright = {
-      disableOrganizeImports = true,
-    },
-    python = {
-      analysis = {
-        ignore = { "*" },
+local configs = {
+  lua_ls = {
+    settings = {
+      Lua = {
+        runtime = {
+          version = "LuaJIT",
+          pathStrict = true,
+          path = { "?.lua", "?/init.lua" },
+        },
+        workspace = {
+          library = vim.list_extend(vim.api.nvim_get_runtime_file("lua", true), {
+            "${3rd}/luv/library",
+            "${3rd}/busted/library",
+            "${3rd}/luassert/library",
+          }),
+          checkThirdParty = "Disable",
+        },
       },
-      pythonPath = python_venv_path,
     },
   },
-})
-
-lspconfig["clangd"].setup({
-  capabilities = capabilities,
-})
-
-lspconfig["ruff"].setup({})
-
-vim.g.markdown_fenced_languages = {
-  "ts=typescript",
+  pyright = {
+    settings = {
+      pyright = {
+        disableOrganizeImports = true,
+      },
+      python = {
+        analysis = {
+          ignore = { "*" },
+        },
+        pythonPath = python_venv_path,
+      },
+    },
+  },
+  clangd = {},
+  ruff = {},
+  denols = {},
+  marksman = {},
+  nimls = {
+    cmd = { "nimlsp", "--stdin ", "--trace" }, -- `nimlsp` の実行コマンド
+    filetypes = { "nim" }, -- Nimファイルに適用
+    root_dir = lspconfig.util.root_pattern("*.nim", ".git"),
+  },
 }
 
-lspconfig["denols"].setup({
+local default_options = {
   capabilities = capabilities,
-})
+}
 
-lspconfig["marksman"].setup({
-  capabilities = capabilities,
-})
+for name, config in pairs(configs) do
+  local new_config = {}
 
-lspconfig["nimls"].setup({
-  capabilities = capabilities,
-  cmd = { "nimlsp", "--stdin ", "--trace" }, -- `nimlsp` の実行コマンド
-  filetypes = { "nim" }, -- Nimファイルに適用
-  root_dir = lspconfig.util.root_pattern("*.nim", ".git"),
-})
+  for k, v in pairs(default_options) do
+    new_config[k] = v
+  end
+
+  for k, v in pairs(config) do
+    new_config[k] = v
+  end
+
+  lspconfig[name].setup(new_config)
+end
