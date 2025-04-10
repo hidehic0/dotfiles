cb7b40f8763b25b67be647d10fcbc1146b760337
hidehic0 <hidehico.ooba@gmail.com>
2025-04-10 21:39:34 +0900

    snacks.notifyをnvim-notifyに変更 noice.nvimを消した

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -218,7 +218,7 @@ return {
     "folke/snacks.nvim",
     event = "VeryLazy",
     config = function()
-      require("plugins.config.snacks")
+      -- require("plugins.config.snacks")
     end,
   },
 }
