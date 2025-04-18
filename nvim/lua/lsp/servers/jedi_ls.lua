97fe655bc95cc4ff1c2133db3e47cd4342ffd3a4
hidehic0 <hidehico.ooba@gmail.com>
2025-04-18 09:15:28 +0900

    neovim: jedi_lsの設定を変更

--- nvim/lua/lsp/servers/jedi_ls.lua
+++ nvim/lua/lsp/servers/jedi_ls.lua
@@ -8,4 +8,12 @@ return {
     "requirements.txt",
     "Pipfile",
   },
+  init_options = {
+    diagnostics = {
+      enable = false, -- 診断（Lint）を無効化
+    },
+    workspace = {
+      environmentPath = vim.fn.exepath("python3") or vim.fn.exepath("python"),
+    },
+  },
 }
