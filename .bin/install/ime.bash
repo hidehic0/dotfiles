3190c4141866bdb4889c990d2ad2e0fd29d59df2
hidehic0 <hidehico.ooba@gmail.com>
2025-04-20 16:17:44 +0900

    小修正

--- .bin/install/ime.bash
+++ .bin/install/ime.bash
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 paru -S fcitx5-im fcitx5-qt fcitx5-gtk fcitx5-configtool
-paru -S fcitx5-skk yaskkserv2-bin
+paru -S fcitx5-skk yaskkserv2-bin rust
+
+cd /tmp
+git clone https://github.com/wachikun/yaskkserv2.git
+cd yaskkserv2
+cargo build --release
+sudo cp -av target/release/yaskkserv2 /sbin/
+sudo cp -av target/release/yaskkserv2_make_dictionary /sbin/
+cd ..
+rm -rf yaskkserv2
 
 mkdir -p $HOME/.local/lib/yaskkserv2/
 sudo yaskkserv2_make_dictionary --dictionary-filename=/usr/local/share/dictionary.yaskkserv2 /usr/share/skk/SKK-JISYO.L /usr/share/skk/SKK-JISYO.jinmei /usr/share/skk/SKK-JISYO.fullname /usr/share/skk/SKK-JISYO.geo /usr/share/skk/SKK-JISYO.propernoun /usr/share/skk/SKK-JISYO.station
