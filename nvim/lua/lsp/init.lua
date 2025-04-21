4710e9455484e32861e91a16aeb9c396ab6c55c0
hidehic0 <hidehico.ooba@gmail.com>
2025-04-21 09:56:11 +0900

    neovim: jediよりpylspの方が安定しているので切り替え

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,5 +1,5 @@
 local config_helper = require("lsp.utils").vim_lsp_config_helper
-local servers = { "lua_ls", "jedi_ls", "ruff", "clangd", "denols", "marksman" }
+local servers = { "lua_ls", "pylsp", "ruff", "clangd", "denols", "marksman" }
 
 local capabilities = require("ddc_source_lsp").make_client_capabilities()
 vim.lsp.config("*", {
---
+++ nvim/lua/lsp/servers/pylsp.lua
@@ -0,0 +1,23 @@
+return {
+  cmd = { "pylsp" },
+  filetypes = { "python" },
+  root_markers = {
+    "pyproject.toml",
+    "setup.py",
+    "setup.cfg",
+    "requirements.txt",
+    "Pipfile",
+  },
+  settings = {
+    pylsp = {
+      plugins = {
+        pylint = { enabled = false }, -- pylintを無効化
+        pyflakes = { enabled = false }, -- pyflakesを無効化
+        pycodestyle = { enabled = false }, -- pycodestyleを無効化
+        mccabe = { enabled = false }, -- mccabeを無効化（必要に応じて）
+        flake8 = { enabled = false }, -- flake8を無効化（必要に応じて）
+        pydocstyle = { enabled = false }, -- pydocstyleを無効化
+      },
+    },
+  },
+}
