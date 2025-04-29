81ff9cf2b98e1e5e226b25840d7156a2ab5f993a
hidehic0 <hidehico.ooba@gmail.com>
2025-04-29 10:52:32 +0900

    add(skk): 絵文字辞書を追加した

--- .bin/install/ime.bash
+++ .bin/install/ime.bash
@@ -9,11 +9,14 @@ cd yaskkserv2
 cargo build --release
 sudo cp -av target/release/yaskkserv2 /sbin/
 sudo cp -av target/release/yaskkserv2_make_dictionary /sbin/
+
+wget https://raw.githubusercontent.com/uasi/skk-emoji-jisyo/master/SKK-JISYO.emoji.utf8
+sudo mv SKK-JISYO.emoji.utf8 /usr/share/skk/
 cd ..
 rm -rf yaskkserv2
 
 mkdir -p $HOME/.local/lib/yaskkserv2/
-sudo yaskkserv2_make_dictionary --dictionary-filename=/usr/local/share/dictionary.yaskkserv2 /usr/share/skk/SKK-JISYO.L /usr/share/skk/SKK-JISYO.jinmei /usr/share/skk/SKK-JISYO.fullname /usr/share/skk/SKK-JISYO.geo /usr/share/skk/SKK-JISYO.propernoun /usr/share/skk/SKK-JISYO.station
+sudo yaskkserv2_make_dictionary --dictionary-filename=$HOME/.local/lib/yaskkserv2/ /usr/share/skk/SKK-JISYO.L /usr/share/skk/SKK-JISYO.jinmei /usr/share/skk/SKK-JISYO.fullname /usr/share/skk/SKK-JISYO.geo /usr/share/skk/SKK-JISYO.propernoun /usr/share/skk/SKK-JISYO.station /usr/share/skk/SKK-JISYO.emoji.utf8
 
 mkdir -p $HOME/.local/share/fcitx5/skk
-echo "type=file,file=$FCITX_CONFIG_DIR/skk/user.dict,mode=readwrite,complete=true\ntype=server,host=localhost,port=1178" >$HOME/.local/share/fcitx5/skk/dictionary_list
+echo "type=file,file=~/.local/share/fcitx5/skk/user.dict,mode=readwrite,complete=true\ntype=server,host=localhost,port=1178" >$HOME/.local/share/fcitx5/skk/dictionary_list
