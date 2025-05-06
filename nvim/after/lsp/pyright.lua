db1c67ef9b96fa1886ad43644ff7f3d3fa332ae8
hidehic0 <hidehico.ooba@gmail.com>
2025-05-06 14:46:55 +0900

    update(neovim): add pythonpath config in pyright settings

--- nvim/after/lsp/pyright.lua
+++ nvim/after/lsp/pyright.lua
@@ -1,3 +1,5 @@
+local python_venv_path = vim.fn.system("which python"):gsub("\n", "")
+
 return {
   settings = {
     pyright = {
@@ -5,6 +7,7 @@ return {
       disableOrganizeImports = true,
     },
     python = {
+      pythonPath = python_venv_path,
       analysis = {
         -- Ignore all files for analysis to exclusively use Ruff for linting
         ignore = { "*" },
