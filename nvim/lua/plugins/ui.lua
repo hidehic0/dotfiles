9147f62101f7dff9e6f3c4c97d993afa553e404f
hidehic0 <hidehico.ooba@gmail.com>
2025-05-06 14:27:42 +0900

    feat(neovim): add tiny-inline-diagnostic.nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -176,4 +176,20 @@ return {
     event = "LspAttach",
     opts = {},
   },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      require("tiny-inline-diagnostic").setup({
+        options = {
+          multilines = {
+            enabled = true,
+            always_show = true,
+          },
+        },
+      })
+      vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+    end,
+  },
 }
