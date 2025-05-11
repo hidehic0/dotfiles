c30c3c2eb2ab26bca2b9f73a26551f994575d0e1
hidehic0 <hidehico.ooba@gmail.com>
2025-05-11 12:25:24 +0900

    update(install_script): updated paru install_script

--- .bin/install/paru.bash
+++ .bin/install/paru.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# paru install
+cd /tmp
 sudo pacman -Syyu
 sudo pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/paru-bin.tar.gz
+tar zxvf paru-bin.tar.gz
+cd paru-bin
+makepkg
+echo "run: sudo pacman \$pkgname-\$pkgver-\$pkgrel-\$arch.pkg.tar.xz"
