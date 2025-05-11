c66b777d99b8f1d1b70e1d2561fe3a0d413bf2ee
hidehic0 <hidehico.ooba@gmail.com>
2025-05-11 09:45:13 +0900

    add(neovim): Center the screen on move

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -10,3 +10,8 @@ vim.api.nvim_set_keymap("n", "x", '"_x', { noremap = true, silent = true })
 
 -- 競プロ用コードを全コピー
 vim.api.nvim_set_keymap("n", "<leader>a", "GyggG", { noremap = true, silent = true })
+
+-- Center the screen on move
+for _, rhs in pairs({ "gg", "G", "h", "l", "j", "k", "<C-d>", "<C-u>" }) do
+  vim.keymap.set("n", rhs, rhs .. "zz", { noremap = true })
+end
