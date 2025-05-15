24287a6276691fddcae6653db171ddfe48668d7d
hidehic0 <hidehico.ooba@gmail.com>
2025-05-15 18:28:19 +0900

    feat: installed ghq

--- zsh/zsh.d/func.zsh
+++ zsh/zsh.d/func.zsh
@@ -11,3 +11,14 @@ cfd() {
 #   \cd $@ && ls
 # }
 # alias cd='custom_cd'
+
+function ghq-fzf() {
+  local src=$(ghq list | fzf --preview "ls /$(ghq root)/{}")
+  if [ -n "$src" ]; then
+    BUFFER="cd $(ghq root)/$src"
+    zle accept-line
+  fi
+  zle -R -c
+}
+zle -N ghq-fzf
+bindkey '^g' ghq-fzf
