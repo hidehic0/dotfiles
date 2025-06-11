4edeb319b6a3465c500a62cc380ef2a75aadcd87
hidehic0 <hidehico.ooba@gmail.com>
2025-06-11 16:29:52 +0900

    feat(neovim): add emacs like keymaps to command line

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -15,3 +15,11 @@ vim.api.nvim_set_keymap("n", "<leader>a", "GyggG", { noremap = true, silent = tr
 for _, rhs in pairs({ "gg", "G", "h", "l", "j", "k", "<C-d>", "<C-u>" }) do
   vim.keymap.set("n", rhs, rhs .. "zz", { noremap = true })
 end
+
+-- コマンドライン用キーマップ
+require("utils").keymaps_set({
+  { mode = "c", lhs = "<C-a>", rhs = "<Home>" },
+  { mode = "c", lhs = "<C-e>", rhs = "<End>" },
+  { mode = "c", lhs = "<C-f>", rhs = "<Right>" },
+  { mode = "c", lhs = "<C-b>", rhs = "<Left>" },
+})
