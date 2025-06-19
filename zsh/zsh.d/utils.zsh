5cde5272d62bedc0191fe29900e1b9dd71f47b28
hidehic0 <hidehico.ooba@gmail.com>
2025-06-19 20:15:08 +0900

    fix

--- zsh/zsh.d/utils.zsh
+++ zsh/zsh.d/utils.zsh
@@ -1,14 +1,6 @@
 #!/bin/zsh
 setopt globdots
 
-zstyle ':completion::complete:*' gain-privileges 1
-zstyle ':fzf-tab:*' fzf-command fzf
-zstyle ':fzf-tab:*' fzf-preview 'echo {}'
-zstyle ':completion:*:descriptions' format '[%d]'
-zstyle ':completion:*' menu no
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'lsd -1 --color=always $realpath'
-
 # zoxide
 # eval "$(zoxide init zsh)"
 
