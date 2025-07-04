35aa77ea998ee10725653cd96fe7bd152ade3e80
hidehic0 <hidehico.ooba@gmail.com>
2025-07-04 10:17:10 +0900

    feat(font): フォールバックフォントにhackgenを使う

---
+++ .bin/install/hackgen.bash
@@ -0,0 +1,5 @@
+cd /tmp
+wget https://github.com/yuru7/HackGen/releases/download/v2.10.0/HackGen_v2.10.0.zip
+unzip HackGen_v2.10.0.zip
+sudo mv HackGen_v2.10.0/ /usr/share/fonts/TTF/
+sudo fc-cache -fv
