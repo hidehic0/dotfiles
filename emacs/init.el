2b4b2f55510da06781f78993dc9c6a2b3742857d
hidehic0 <hidehico.ooba@gmail.com>
2025-08-07 19:52:16 +0900

    feat(emacs): すこし設定を作ってみた

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,43 @@
-(setenv "PATH" (concat (getenv "PATH") ":" (expand-file-name "$HOME/.local/share/aquaproj-aqua/bin")))
-(setenv "AQUA_GLOBAL_CONFIG" "$HOME/.dotfiles/aqua.yaml")
-(add-to-list 'exec-path (expand-file-name "~/.local/share/aquaproj-aqua/bin"))
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("org" . "https://orgmode.org/elpa/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("gnu" . "https://elpa.gnu.org/packages/")))
+  (package-initialize)
+  (unless package-archive-contents
+    (package-refresh-contents))
+  (unless (package-installed-p 'leaf)
+    (package-install 'leaf)))
+
+(leaf mise :ensure t
+  :config
+  (require 'mise)
+  (add-hook 'prog-mode-hook #'mise-mode)
+  (add-hook 'text-mode-hook #'mise-mode)
+  )
+
+
+
+
+(leaf llama :ensure t)
+(leaf inheritenv :ensure t)
+
+(leaf evil :ensure t
+  :leaf-defer nil
+  :commands evil-define-key
+  :init
+
+  (evil-mode 1)
+
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-integration t)
+  )
+
+(leaf evil-escape :ensure t :after evil
+:init
+
+( evil-escape-mode 1)
+(setq-default evil-escape-key-sequence "jj")
+
+(setq-default evil-escape-delay 0.5)
+      )
