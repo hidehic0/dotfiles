7cd349fc46d5b98b68e5c93bb9b669ffe765ac33
hidehic0 <hidehico.ooba@gmail.com>
2025-09-25 17:44:28 +0900

    feat(vim): dpp.vimを導入 並びにプラグインも導入

--- vim/dpp.ts
+++ vim/dpp.ts
@@ -1,166 +1,110 @@
+import type {
+  ContextBuilder,
+  Dpp,
+  Plugin,
+} from "https://deno.land/x/dpp_vim@v0.0.9/types.ts";
 import {
-	type ContextBuilder,
-	type Plugin,
-} from "jsr:@shougo/dpp-vim@~5.0.0/types";
-import {
-	BaseConfig,
-	type ConfigReturn,
-	type MultipleHook,
-} from "jsr:@shougo/dpp-vim@~5.0.0/config";
-import type {
-	Ext as TomlExt,
-	Params as TomlParams,
-} from "jsr:@shougo/dpp-ext-toml@~1.3.0";
-import type {
-	Ext as LazyExt,
-	LazyMakeStateResult,
-	Params as LazyParams,
-} from "jsr:@shougo/dpp-ext-lazy@~1.5.0";
-import type {
-	Ext as LocalExt,
-	Params as LocalParams,
-} from "jsr:@shougo/dpp-ext-local@~1.3.0";
+  BaseConfig,
+  type ConfigReturn,
+} from "https://deno.land/x/dpp_vim@v0.0.9/types.ts";
 
-import type { Denops } from "jsr:@denops/std@~8.0.0";
-import * as fn from "jsr:@denops/std@~8.0.0/function";
+import type { Denops } from "https://deno.land/x/dpp_vim@v0.0.9/deps.ts";
+
+type Toml = {
+  hooks_file?: string;
+  ftplugins?: Record<string, string>;
+  plugins?: Plugin[];
+};
+
+type LazyMakeStateResult = {
+  plugins: Plugin[];
+  stateLines: string[];
+};
+
+type CheckFile = {
+  path: string;
+  lazy: boolean;
+};
 
 export class Config extends BaseConfig {
-	override async config(args: {
-		denops: Denops;
-		contextBuilder: ContextBuilder;
-		basePath: string;
-	}): Promise<ConfigReturn> {
-		args.contextBuilder.setGlobal({
-			protocols: ["git"],
-		});
+  override async config(args: {
+    denops: Denops;
+    contextBuilder: ContextBuilder;
+    basePath: string;
+    dpp: Dpp;
+  }): Promise<ConfigReturn> {
+    args.contextBuilder.setGlobal({
+      extParams: {
+        installer: {
+          checkDiff: true,
+        },
+      },
+      protocols: ["git"],
+    });
 
-		const [context, options] = await args.contextBuilder.get(args.denops);
+    const checkFiles: CheckFile[] = [
+      {
+        path: "~/.vim/dpp.toml",
+        lazy: false,
+      },
+      {
+        path: "~/.vim/dpp_lazy.toml",
+        lazy: true,
+      },
+    ];
 
-		const recordPlugins: Record<string, Plugin> = {};
-		const ftplugins: Record<string, string> = {};
-		const hooksFiles: string[] = [];
-		let multipleHooks: MultipleHook[] = [];
+    const [context, options] = await args.contextBuilder.get(args.denops);
 
-		// Load toml plugins
-		const [tomlExt, tomlOptions, tomlParams]: [
-			TomlExt | undefined,
-			ExtOptions,
-			TomlParams,
-		] = await args.denops.dispatcher.getExt(
-			"toml",
-		) as [TomlExt | undefined, ExtOptions, TomlParams];
-		if (tomlExt) {
-			const action = tomlExt.actions.load;
+    const tomls: Toml[] = [];
 
-			const tomlPromises = [
-				{ path: "$BASE_DIR/dpp.toml", lazy: false },
-			].map((tomlFile) =>
-				action.callback({
-					denops: args.denops,
-					context,
-					options,
-					protocols,
-					extOptions: tomlOptions,
-					extParams: tomlParams,
-					actionParams: {
-						path: tomlFile.path,
-						options: {
-							lazy: tomlFile.lazy,
-						},
-					},
-				})
-			);
+    for (const file of checkFiles) {
+      tomls.push(
+        await args.dpp.extAction(
+          args.denops,
+          context,
+          options,
+          "toml",
+          "load",
+          {
+            path: file.path,
+            options: {
+              lazy: file.lazy,
+            },
+          },
+        ) as Toml,
+      );
+    }
 
-			const tomls = await Promise.all(tomlPromises);
+    const recordPlugins: Record<string, Plugin> = {};
+    const hooksFiles: string[] = [];
 
-			// Merge toml results
-			for (const toml of tomls) {
-				for (const plugin of toml.plugins ?? []) {
-					recordPlugins[plugin.name] = plugin;
-				}
+    for (const toml of tomls) {
+      if (toml.plugins) {
+        for (const plugin of toml.plugins) {
+          recordPlugins[plugin.name] = plugin;
+        }
+      }
 
-				if (toml.ftplugins) {
-					mergeFtplugins(ftplugins, toml.ftplugins);
-				}
+      if (toml.hooks_file) {
+        hooksFiles.push(toml.hooks_file);
+      }
+    }
 
-				if (toml.multiple_hooks) {
-					multipleHooks = multipleHooks.concat(toml.multiple_hooks);
-				}
+    const lazyResult = await args.dpp.extAction(
+      args.denops,
+      context,
+      options,
+      "lazy",
+      "makeState",
+      {
+        plugins: Object.values(recordPlugins),
+      },
+    ) as LazyMakeStateResult | undefined;
 
-				if (toml.hooks_file) {
-					hooksFiles.push(toml.hooks_file);
-				}
-			}
-		}
-
-		const [localExt, localOptions, localParams]: [
-			LocalExt | undefined,
-			ExtOptions,
-			LocalParams,
-		] = await args.denops.dispatcher.getExt(
-			"local",
-		) as [LocalExt | undefined, ExtOptions, LocalParams];
-		if (localExt) {
-			const action = localExt.actions.local;
-
-			const localPlugins = await action.callback({
-				denops: args.denops,
-				context,
-				options,
-				protocols,
-				extOptions: localOptions,
-				extParams: localParams,
-				actionParams: {
-					directory: "~/work",
-					options: {
-						frozen: true,
-						merged: false,
-					},
-				},
-			});
-
-			for (const plugin of localPlugins) {
-				if (plugin.name in recordPlugins) {
-					recordPlugins[plugin.name] = Object.assign(
-						recordPlugins[plugin.name],
-						plugin,
-					);
-				} else {
-					recordPlugins[plugin.name] = plugin;
-				}
-			}
-		}
-
-		const [lazyExt, lazyOptions, lazyParams]: [
-			LazyExt | undefined,
-			ExtOptions,
-			LazyParams,
-		] = await args.denops.dispatcher.getExt(
-			"lazy",
-		) as [LazyExt | undefined, ExtOptions, PackspecParams];
-		let lazyResult: LazyMakeStateResult | undefined = undefined;
-		if (lazyExt) {
-			const action = lazyExt.actions.makeState;
-
-			lazyResult = await action.callback({
-				denops: args.denops,
-				context,
-				options,
-				protocols,
-				extOptions: lazyOptions,
-				extParams: lazyParams,
-				actionParams: {
-					plugins: Object.values(recordPlugins),
-				},
-			});
-		}
-
-		return {
-			ftplugins,
-			hooksFiles,
-			multipleHooks,
-			plugins: lazyResult?.plugins ?? [],
-			stateLines: lazyResult?.stateLines ?? [],
-		};
-	}
+    return {
+      hooksFiles,
+      plugins: lazyResult?.plugins ?? [],
+      stateLines: lazyResult?.stateLines ?? [],
+    };
+  }
 }
