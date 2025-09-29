80c15a1ba0b8a2f89d677fcb074dc05639425fc5
hidehic0 <hidehico.ooba@gmail.com>
2025-09-29 21:32:10 +0900

    lspをセットアップ

---
+++ nvim/hooks/lsp.lua
@@ -0,0 +1,23 @@
+-- lua_source {{{
+-- vim.lsp.config("*", {
+-- capabilities = require("ddc_source_lsp").make_client_capabilities(),
+--})
+
+vim.lsp.enable({
+  "lua_ls",
+  "pyright",
+  "ruff",
+  "clangd",
+  "denols",
+  -- "marksman",
+  "tombi",
+  "nim_langserver",
+  "gopls",
+  "nil_ls",
+  "ts_ls",
+  "rust_analyzer",
+  "yamlls",
+  "efm",
+  -- "markdown_oxide",
+})
+-- }}}
