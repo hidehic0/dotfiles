8626e814602380f5456a49d89b136ae3f50a42b7
hidehic0 <hidehico.ooba@gmail.com>
2025-09-29 21:16:30 +0900

    deol.nvimをインストール

---
+++ nvim/hooks/deol.lua
@@ -0,0 +1,31 @@
+-- lua_source {{{
+local fn = require("artemis").fn
+local keymaps_set = require("utils").keymaps_set
+
+fn.deol.set_option({
+  split = "vertical",
+  toggle = true,
+  winwidth = 75,
+})
+
+vim.keymap.set("n", "<C-t>", function()
+  fn.deol.start()
+end, { noremap = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "deol",
+  callback = function()
+    keymaps_set({
+      { mode = "t", lhs = "<Esc>", rhs = "<C-\\><C-n>", { buffer = true } },
+      {
+        mode = { "n", "t" },
+        lhs = "<C-t>",
+        rhs = function()
+          fn.deol.start()
+        end,
+        opts = { noremap = true },
+      },
+    })
+  end,
+})
+-- }}}
