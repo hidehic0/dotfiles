86a0e0c48b318383dcf45b2fdbd7c7b9ece2e6ce
hidehic0 <hidehico.ooba@gmail.com>
2025-10-13 16:28:35 +0900

    fix(neovim): dpp.vimのロード位置を変更

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,9 @@ if vim.loader then
   vim.loader.enable()
 end
 
+require("core.dpp")
 require("core.keymaps")
 require("core.options")
 require("core.indent")
-require("core.dpp")
 
 vim.cmd("syntax on")
