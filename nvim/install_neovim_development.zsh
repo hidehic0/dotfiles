fb9b3cd20c438ba2f320a8492be08f2fe2d98448
hidehic0 <hidehico.ooba@gmail.com>
2025-10-15 13:51:38 +0900

    feat(neovim): neovimのインストールスクリプトでビルドするようにした

--- nvim/install_neovim_development.zsh
+++ nvim/install_neovim_development.zsh
@@ -1,26 +1,12 @@
 #!/bin/zsh
 
-cd ~
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.tar.gz
-tar zxvf nvim-linux-x86_64.tar.gz
-rm nvim-linux-x86_64.tar.gz
-
-sudo rm -rf /usr/local/bin/nvim-linux-x86_64 /usr/local/bin/nvim
-sudo mv nvim-linux-x86_64 /usr/local/bin/
-sudo ln -s /usr/local/bin/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
-
-while getopts ":a" optKey; do
-  case "$optKey" in
-  a)
-    wget https://github.com/vadimcn/codelldb/releases/download/v1.11.4/codelldb-linux-x64.vsix
-    unzip codelldb-linux-x64.vsix
-    sudo mv extension /usr/local/bin/codelldb_e
-    sudo rm /usr/local/bin/codelldb
-    sudo ln -s /usr/local/bin/codelldb_e/adapter/codelldb /usr/local/bin/codelldb
-    rm \[Content_Types\].xml codelldb-linux-x64.vsix extension.vsixmanifest
-    ;;
-  esac
-done
+cd /tmp
+git clone https://github.com/neovim/neovim.git --depth 1
+cd neovim
+sudo make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=/usr/local
+sudo make install
+cd ..
+sudo rm -rf neovim
 
 # plugpath="/usr/local/bin/nvim-linux-x86_64/share/nvim/runtime/plugin"
 # sudo rm $plugpath/gzip.vim $plugpath/matchit.vim $plugpath/matchparen.vim $plugpath/shada.vim $plugpath/spellfile.vim $plugpath/tutor.vim $plugpath/zipPlugin.vim $plugpath/tohtml.lua $plugpath/tohtml.lua /usr/local/bin/nvim-linux-x86_64/share/nvim/runtime/ftplugin.vim
