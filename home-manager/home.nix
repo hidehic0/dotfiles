4e0dab92d542350568693e2e07b6957fb94cdafd
hidehic0 <hidehico.ooba@gmail.com>
2025-12-21 18:16:49 +0900

    feat(nix): 初期設定

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,74 +1,9 @@
 {
-  inputs,
-  lib,
-  config,
-  pkgs,
-  ...
-}:
-let 
-  username = "hidehico";
-  unstable = import <nixpkgs-unstable> {};
-in {
-  nixpkgs = {
-    overlays = [
-      # (import (builtins.fetchTarball {
-      #   url = "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
-      # }))
-      (import (builtins.fetchTarball {
-        url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-      }))
-    ];
-    config = {
-      allowUnfree=true;
-    };
+  home = rec {
+    username = "hidehic0";
+    homeDirectory = "/home/${username}";
+    stateVersion="25.11";
   };
 
-  home = {
-    username = "hidehico";
-    homeDirectory = "/home/hidehico";
-
-    stateVersion = "24.11";
-    packages = with unstable;
-    [
-      # aria2
-      # black
-      # delta
-      # deno
-      # emacs30
-      # fish
-      # fzf
-      # gh
-      # hugo
-      # isort
-      # lazydocker
-      # lazygit
-      # lua-language-server
-      # lua54Packages.luacheck
-      # markdownlint-cli2
-      # marksman
-      # mise
-      # online-judge-tools
-      # ruby
-      # ruff
-      # sheldon
-      # shellcheck
-      # shfmt
-      # taplo
-      # tree-sitter
-      # typescript-language-server
-      # vim
-      # vim-startuptime
-      # yamlfmt
-      # zoxide
-    ];
-
-    file = {
-    };
-
-    sessionVariables = {
-    };
-
-    enableNixpkgsReleaseCheck = false;
-  };
   programs.home-manager.enable = true;
 }
