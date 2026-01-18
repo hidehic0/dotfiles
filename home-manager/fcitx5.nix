0d148fb68b7b6daa4dcd5583ece99a698c18c38b
hidehic0 <hidehico.ooba@gmail.com>
2026-01-18 15:12:51 +0900

    format

--- home-manager/fcitx5.nix
+++ home-manager/fcitx5.nix
@@ -1,27 +1,27 @@
-{ pkgs,... }:
+{ pkgs, ... }:
 {
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [ 
-      fcitx5-skk 
+    fcitx5.addons = with pkgs; [
+      fcitx5-skk
       fcitx5-gtk
       libsForQt5.fcitx5-qt
       catppuccin-fcitx5
 
     ];
-};
+  };
   home.packages = with pkgs; [
     yaskkserv2
   ];
   systemd.user.services.yaskkserv2 = {
     Install = {
-      WantedBy=["default.target"];
+      WantedBy = [ "default.target" ];
     };
     Service = {
-      Type="oneshot";
-      ExecStart = "${pkgs.yaskkserv2}/bin/yaskkserv2 --google-japanese-input=last --google-suggest ${pkgs.yaskkserv2}/share/dictionary.yaskkserv2";
-      RemainAfterExit="yes";
+      Type = "oneshot";
+      ExecStart = "${pkgs.yaskkserv2}/bin/yaskkserv2 ${pkgs.yaskkserv2}/share/dictionary.yaskkserv2";
+      RemainAfterExit = "yes";
     };
   };
 }
