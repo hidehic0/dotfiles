c2de944cdf6ea6c2f237a71d3836e00f8ff9d7b6
hidehic0 <hidehico.ooba@gmail.com>
2026-02-13 20:58:46 +0900

    feat(nixos): tlpをインストール

--- nixos/modules/power_profile.nix
+++ nixos/modules/power_profile.nix
@@ -1,4 +1,20 @@
 { ... }:
 {
-  services.power-profiles-daemon.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 65;
+
+      WIFI_PWR_ON_BAT = "off"; # これないとnetworkが終わる
+
+      START_CHARGE_THRESH_BAT0 = 40;
+      STOP_CHARGE_THRESH_BAT0 = 70;
+    };
+  };
 }
