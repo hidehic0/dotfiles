79b0310de6ce540d2355a727bdfcfc6bbf4caeb2
hidehic0 <hidehico.ooba@gmail.com>
2026-02-23 13:37:59 +0900

    feat(neovim): barbar.nvimに乗り換え

---
+++ nvim/hooks/barbar.lua
@@ -0,0 +1,31 @@
+-- lua_source {{{
+require("barbar").setup({
+  animation = true,
+
+  icons = {
+    -- Configure the base icons on the bufferline.
+    -- Valid options to display the buffer index and -number are `true`, 'superscript' and 'subscript'
+    buffer_index = false,
+    buffer_number = false,
+    button = "",
+    -- Enables / disables diagnostic symbols
+    diagnostics = {
+      [vim.diagnostic.severity.ERROR] = { enabled = true, icon = " " },
+      [vim.diagnostic.severity.WARN] = { enabled = true, icon = " " },
+      [vim.diagnostic.severity.INFO] = { enabled = false },
+      [vim.diagnostic.severity.HINT] = { enabled = false },
+    },
+    pinned = { button = "󰐃", filename = true },
+  },
+})
+
+vim.keymap.set("n", "gT", "<Cmd>BufferPrevious<CR>")
+vim.keymap.set("n", "gt", "<Cmd>BufferNext<CR>")
+vim.keymap.set("n", "<leader><", "<Cmd>BufferMovePrevious<CR>")
+vim.keymap.set("n", "<leader>>", "<Cmd>BufferMoveNext<CR>")
+vim.keymap.set("n", "<leader>q", "<Cmd>BufferClose<CR>")
+-- }}}
+
+-- lua_add {{{
+vim.g.barbar_auto_setup = false
+-- }}}
