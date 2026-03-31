15324f15efeb96157c8719c7d423bb847f70e375
hidehic0 <hidehico.ooba@gmail.com>
2026-03-31 21:25:33 +0900

    update(neovim): Obsidian newでファイル名にhashを入れる

--- nvim/hooks/obsidian.lua
+++ nvim/hooks/obsidian.lua
@@ -7,6 +7,21 @@ require("obsidian").setup({
   daily_notes = {
     folder = "dailies",
   },
+  note_id_func = function(title)
+    -- 参考: https://zenn.dev/mimikun/articles/using-obsidian-nvim
+    local prefix = tostring(os.time())
+    local suffix = ""
+
+    if title ~= nil then
+      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    else
+      for _ = 1, 4 do
+        suffix = suffix .. string.char(math.random(65, 90))
+      end
+    end
+
+    return prefix .. "-" .. suffix
+  end,
 })
 
 vim.keymap.set("n", "<leader>oo", "<Cmd>Obsidian open<CR>")
