c4902a49fc1866c024fc4da0dff8e151b4867bef
hidehic0 <hidehico.ooba@gmail.com>
2026-04-10 21:15:17 +0900

    update(home-manaegr): formated home-manager/neovim.nix

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -1,26 +1,34 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
+let
+  kakehashi = inputs.kakehashi.packages.${pkgs.stdenv.hostPlatform.system}.default;
+in
 {
-  home.packages = with pkgs; [
-    neovim
-    deno
-    ripgrep
-    lua-language-server
-    efm-langserver
-    tombi
-    shellcheck
-    actionlint
-    eslint
-    shfmt
-    jq
-    tree-sitter
-    ty
-    nil
-    yaml-language-server
-    rust-analyzer
-    gopls
-    stylua
-    ruff
-    oxlint
-    oxfmt
-  ];
+  home.packages =
+    with pkgs;
+    [
+      neovim
+      deno
+      ripgrep
+      lua-language-server
+      efm-langserver
+      tombi
+      shellcheck
+      actionlint
+      eslint
+      shfmt
+      jq
+      tree-sitter
+      ty
+      nil
+      yaml-language-server
+      rust-analyzer
+      gopls
+      stylua
+      ruff
+      oxlint
+      oxfmt
+    ]
+    ++ [
+      kakehashi
+    ];
 }
