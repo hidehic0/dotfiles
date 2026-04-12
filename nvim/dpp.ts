d51915f3293c0d6ca02a7621a785ad134fddb992
hidehic0 <hidehico.ooba@gmail.com>
2026-04-12 16:27:27 +0900

    refactor(neovim): dpp.vimの設定でextの関数を使うときactionを使う

--- nvim/dpp.ts
+++ nvim/dpp.ts
@@ -1,18 +1,21 @@
-import type { ContextBuilder, Plugin } from "@shougo/dpp-vim/types";
+import type { ContextBuilder, ExtOptions, Plugin, ProtocolName } from "@shougo/dpp-vim/types";
+import { Protocol } from "@shougo/dpp-vim/protocol";
 import type { Dpp } from "@shougo/dpp-vim/dpp";
 import { BaseConfig, type ConfigReturn } from "@shougo/dpp-vim/config";
 
 import type { Denops } from "@denops/core";
 
+import type { Ext as TomlExt, Params as TomlParams } from "@shougo/dpp-ext-toml";
+import type {
+  Ext as LazyExt,
+  LazyMakeStateResult,
+  Params as LazyParams,
+} from "@shougo/dpp-ext-lazy";
+
 type Toml = {
   hooks_file?: string;
   ftplugins?: Record<string, string>;
   plugins?: Plugin[];
-};
-
-type LazyMakeStateResult = {
-  plugins: Plugin[];
-  stateLines: string[];
 };
 
 type CheckFile = {
@@ -39,6 +42,14 @@ export class Config extends BaseConfig {
       protocols: ["git"],
     });
 
+    const [context, options] = await args.contextBuilder.get(args.denops);
+    const protocols = (await args.denops.dispatcher.getProtocols()) as Record<
+      ProtocolName,
+      Protocol
+    >;
+    const recordPlugins: Record<string, Plugin> = {};
+    const hooksFiles: string[] = [];
+
     const checkFiles: CheckFile[] = [
       {
         path: "~/.config/nvim/dpp.toml",
@@ -53,46 +64,68 @@ export class Config extends BaseConfig {
       });
     }
 
-    const [context, options] = await args.contextBuilder.get(args.denops);
+    const [tomlExt, tomlOptions, tomlParams]: [TomlExt | undefined, ExtOptions, TomlParams] =
+      (await args.denops.dispatcher.getExt("toml")) as [
+        TomlExt | undefined,
+        ExtOptions,
+        TomlParams,
+      ];
 
-    const tomls: Toml[] = [];
+    if (tomlExt) {
+      const action = tomlExt.actions.load;
 
-    for (const file of checkFiles) {
-      tomls.push(
-        (await args.dpp.extAction(args.denops, context, options, "toml", "load", {
-          path: file.path,
-          options: {
-            lazy: file.lazy,
+      const tomlPromises = checkFiles.map((file) =>
+        action.callback({
+          denops: args.denops,
+          context,
+          options,
+          protocols,
+          extOptions: tomlOptions,
+          extParams: tomlParams,
+          actionParams: {
+            path: file.path,
+            options: {
+              lazy: file.lazy,
+            },
           },
-        })) as Toml,
+        }),
       );
-    }
+      const tomls: Toml[] = await Promise.all(tomlPromises);
 
-    const recordPlugins: Record<string, Plugin> = {};
-    const hooksFiles: string[] = [];
+      for (const toml of tomls) {
+        if (toml.plugins) {
+          for (const plugin of toml.plugins) {
+            recordPlugins[plugin.name] = plugin;
+          }
+        }
 
-    for (const toml of tomls) {
-      if (toml.plugins) {
-        for (const plugin of toml.plugins) {
-          recordPlugins[plugin.name] = plugin;
+        if (toml.hooks_file) {
+          hooksFiles.push(toml.hooks_file);
         }
       }
-
-      if (toml.hooks_file) {
-        hooksFiles.push(toml.hooks_file);
-      }
     }
 
-    const lazyResult = (await args.dpp.extAction(
-      args.denops,
-      context,
-      options,
-      "lazy",
-      "makeState",
-      {
-        plugins: Object.values(recordPlugins),
-      },
-    )) as LazyMakeStateResult | undefined;
+    const [lazyExt, lazyOptions, lazyParams]: [LazyExt | undefined, ExtOptions, LazyParams] =
+      (await args.denops.dispatcher.getExt("lazy")) as [
+        LazyExt | undefined,
+        ExtOptions,
+        LazyParams,
+      ];
+
+    let lazyResult: LazyMakeStateResult | undefined = undefined;
+    if (lazyExt) {
+      lazyResult = await lazyExt.actions.makeState.callback({
+        denops: args.denops,
+        context,
+        options,
+        protocols,
+        extOptions: lazyOptions,
+        extParams: lazyParams,
+        actionParams: {
+          plugins: Object.values(recordPlugins),
+        },
+      });
+    }
 
     return {
       hooksFiles,
