ab5aa39dc81dbce657eca56d426f4cf78d8c4fda
hidehic0 <hidehico.ooba@gmail.com>
2026-05-22 19:48:09 +0900

    update(nixos): tlpの設定を変更

--- nixos/modules/power_profile.nix
+++ nixos/modules/power_profile.nix
@@ -4,12 +4,18 @@
     enable = true;
     settings = {
       CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
 
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 65;
+      CPU_MAX_PERF_ON_BAT = 55;
 
       WIFI_PWR_ON_BAT = "off"; # これないとnetworkが終わる
 
