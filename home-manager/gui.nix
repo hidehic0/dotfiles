ab7e5fb30250af029110a86b68d35a380301a1f1
hidehic0 <hidehico.ooba@gmail.com>
2026-06-19 09:48:17 +0900

    update(home-manager): vivaldiのバージョン固定を解除

--- home-manager/gui.nix
+++ home-manager/gui.nix
@@ -1,35 +1,17 @@
 { pkgs, ... }:
-let
-  vivaldi_snapshot_4063-3 = pkgs.vivaldi.overrideDerivation (
-    dir:
-    let
-      version = "8.0.4033.44";
-    in
-    {
-      inherit version;
-      name = "vivaldi-snapshot-${version}";
-      src = pkgs.fetchurl {
-        url = "https://downloads.vivaldi.com/stable/vivaldi-stable_${version}-1_amd64.deb";
-        hash = "sha256-KzDaOntRz8DOcWX5+GhMyGAr+FIh2RC3S6JEIGaBQfE=";
-      };
-    }
-  );
-in
 {
-  home.packages =
-    with pkgs;
-    [
-      bitwarden-desktop
-      ghostty
-      pcmanfm
-      discord-ptb
-      obsidian
-      gnome-clocks
-      gnome-pomodoro
-      anki
-      easyeffects
-      pinta
-      safeeyes
-    ]
-    ++ [ vivaldi_snapshot_4063-3 ];
+  home.packages = with pkgs; [
+    bitwarden-desktop
+    ghostty
+    pcmanfm
+    discord-ptb
+    obsidian
+    gnome-clocks
+    gnome-pomodoro
+    anki
+    easyeffects
+    pinta
+    safeeyes
+    vivaldi
+  ];
 }
